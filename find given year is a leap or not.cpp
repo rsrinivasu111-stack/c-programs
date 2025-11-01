30780833e761d6cb61bec0fb662a12240f7342f5
rsrinivasu111-stack <rsrinivasu111@gmail.com>
2025-11-01 11:11:18 +0530

    Add files via upload

---
+++ find given year is a leap or not.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int y;
+	printf("enter a year");
+	scanf("%d",&y);
+	if(y%4==0){
+	printf("it is a leap year");
+}
+	else{
+		
+	printf("it is  a not leap year");
+}
+	return 0;
+	
+	
+}
