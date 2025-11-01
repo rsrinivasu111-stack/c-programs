30069dac028b2dfc65bc5fa2ff2d3708669aad58
rsrinivasu111-stack <rsrinivasu111@gmail.com>
2025-11-01 10:42:08 +0530

    Add files via upload

---
+++ bitwise operators.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,and,or;
+    printf("enter x,y vaues");
+    scanf("%d%d",&x,&y);
+    and=x&y;
+    printf("bitwise and=%d",and);
+    or='xly';
+    printf("\nbitwise or=%d",or);
+    return 0;
+}
