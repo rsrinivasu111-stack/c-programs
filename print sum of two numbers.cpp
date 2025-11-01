118a55c7daf1acebd6dc77f5b64b2ab8854f5900
rsrinivasu111-stack <rsrinivasu111@gmail.com>
2025-11-01 11:14:15 +0530

    Add files via upload

---
+++ print sum of two numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (){
+	
+	int num1,num2,sum;
+	
+	printf(" enter the value 1st&2nd values : ");
+	scanf("%d  %d",&num1,&num2);
+	sum=num1+num2;
+	printf("sum =%d",sum);
+	return 0;
+}
