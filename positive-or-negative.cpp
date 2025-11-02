bb911857963ee3ec2914ff4ffec62bf76eeec0f3
rsrinivasu111-stack <rsrinivasu111@gmail.com>
2025-11-02 21:15:03 +0530

    Add files via upload

---
+++ positive-or-negative.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter n value");
+    scanf("%d",&n);
+    n>0?printf("it is positive"):printf("it is negative");
+    getch();
+}
