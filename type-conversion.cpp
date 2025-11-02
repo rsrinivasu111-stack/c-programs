0ba9872639a54d60093f201f8850de2cc39bc79c
rsrinivasu111-stack <rsrinivasu111@gmail.com>
2025-11-02 21:21:26 +0530

    Add files via upload

---
+++ type-conversion.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5,b=2;
+    float c,d;
+    c=a/b;
+    printf("c=%f",c);
+    d=(float)a/b;
+    printf("\nd=%f",d);
+    return 0;
+}
+
